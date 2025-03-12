88eec3a7a5d4d2f4b725a0b83c1909aa349226da
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2025-03-11 21:42:22 -0300

    Atualizando jogoteca

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class jogo:
   def __init__(self, nome, categoria, console):
@@ -12,10 +12,17 @@ jogo3 = jogo('Mortal Kombat', 'Luta', 'PS2')
 lista_jogos = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__) #SIGNIFICA: Cria uma instância do Flask onde podemos criar uma rota com nome do html:
+app.secret_key = 'alura'
 @app.route('/') # definir rota para o servidor
 def index(): # definir função para executar com a rota
     return render_template('lista.html', titulo="Jogos", jogos = lista_jogos)
-
+@app.route('/novo')
+def novo():
+    if 'logado' not in session or session['logado'] == None:
+        print("Usuário não logado, redirecionando para login...")
+        flash('É necessário fazer o login!')
+        return redirect('/login?proxima=novo')
+    return render_template('novo.html', titulo='Novo Jogo') 
 @app.route('/criar', methods = ['POST',])
 def criar():
   nome = request.form['nome']
@@ -25,7 +32,25 @@ def criar():
   lista_jogos.append(jogo_novo)
   return redirect('/') #redireciona para uma rota.
 
-@app.route('/novo')
-def novo():
-  return render_template('novo.html', titulo='Novo Jogo')    
+@app.route('/login')
+def login():
+  proxima = request.args.get('proxima')
+  return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods = ['POST',])
+def autenticar():
+  if 'alohomora' == request.form['senha']:
+    session['logado'] = request.form['usuario'] 
+    proxima_pagina = request.form['proxima'] 
+    return redirect('/{}'.format(proxima_pagina))
+  else:
+    flash(f"Error - {session.get('logado', 'Usuário não logado')}")
+    return redirect('/login')
+
+@app.route('/logout')
+def logout():
+  flash(f"Usuário {session.get('logado', 'Usuário não logado')} deslogado com sucesso!")
+  session['logado'] = None
+  return redirect('/login')
+
 app.run(debug=True) #RUN para rodar o servidor e rota
